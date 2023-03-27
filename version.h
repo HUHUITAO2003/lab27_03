e492ba6e498bea32ce572bf3bbcabdcdfb7fe493
HU_HUI_TAO <huitao.huangelo@gmail.com>
2023-03-27 17:27:55 +0200

    Added version name

---
+++ version.h
@@ -0,0 +1,11 @@
+//
+// Created by Juliet on 27/03/2023.
+//
+
+#ifndef BITMAPIMAGE_CPP_VERSION_H
+#define BITMAPIMAGE_CPP_VERSION_H
+
+#include <string>
+
+static const std::string VERSION = "v1.0";
+#endif //BITMAPIMAGE_CPP_VERSION_H
